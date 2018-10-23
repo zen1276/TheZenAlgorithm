0b60f30e41deb9b1a654c41031a8e74fd774b976
Van hoffman <zvan1276@740s-pl-119.local>
2018-10-23 09:52:49 -0600

    fixed minor errors, such as including a plist, and fixing spelling errors

--- TheZenAlgorithm/Controller/AlgorithmViewController.swift
+++ TheZenAlgorithm/Controller/AlgorithmViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 public class AlgorithmViewController: UIViewController
 {
-    @IBOutlet weak var AlgorithmText: UILabel!
+    @IBOutlet weak var algorithmText: UILabel!
     
     @IBOutlet weak var SwiftImage: UIImageView!
    
@@ -24,6 +24,8 @@ public class AlgorithmViewController: UIViewController
 
     private func formatAlgorithm() -> Void
     {
+        var algorithmSteps : [String] = []
+        
         let title : String = "How to make an app in Xcode"
         
         let stepOne : String = "First, open up Xcode, and click on Create a new Xcode project. For our situation, we will be creating a Single View App, so click on that option."
@@ -32,19 +34,19 @@ public class AlgorithmViewController: UIViewController
         let stepFour : String = "Then create your resources folder, which will include assets.xcassets, info.plist, and appdelegate.swift."
         let stepFive : String = "Finally, create a new repository of this app to github, and click undo so that you can go into the .gitignore where you will add \"*.xcuserstate\""
         
-        let algorithm = [stepOne, stepTwo, stepThree, stepFour, stepFive]
+        algorithmSteps = [stepOne, stepTwo, stepThree, stepFour, stepFive]
         
-        let attributesDictionary = [NSAttributedString.Key.font : algorithmText.font]
-        let full AttributedString = NSMutableAttributedString(string: title, attributes: attributesDictionary)
+        let attributesDictionary = [NSAttributedStringKey.font : algorithmText.font]
+        let fullAttributedString = NSMutableAttributedString(string: title, attributes: attributesDictionary)
         
-        for step in algorithm
+        for step in algorithmSteps
         {
         let bullet : String = "🈸"
             let formattedStep : String = "\n\(bullet) \(step)"
             let attributedStringStep : NSMutableAttributedString = NSMutableAttributedString(string: formattedStep)
             let outlineStyle = createOutlineStyle()
             
-            attributedStringStep.addAttributes([NSAttributedString.Key.paragraphStyle : outlineStyle], range: NSMakeRange(0,attributedStringStep.length))
+            attributedStringStep.addAttributes([NSAttributedStringKey.paragraphStyle : outlineStyle], range: NSMakeRange(0,attributedStringStep.length))
             
             fullAttributedString.append(attributedStringStep)
         }
