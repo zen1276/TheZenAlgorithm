b2951dacc76c895ce2c169aea839e99df6036707
Van hoffman <zvan1276@740s-pl-119.local>
2018-10-23 08:43:32 -0600

    linked the label and image, then  created let variables for the algorithm's helper method

---
+++ TheZenAlgorithm/Controller/AlgorithmViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  TheZenAlgorithm
+//
+//  Created by Van hoffman, Zendan on 10/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class AlgorithmViewController: UIViewController
+{
+    @IBOutlet weak var AlgorithmText: UILabel!
+    
+    @IBOutlet weak var SwiftImage: UIImageView!
+   
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        formatAlgorithm()
+        
+    }
+
+    private func formatAlgorithm() -> Void
+    {
+        let stepOne : String = ""
+        let stepTwo : String = ""
+        let stepThree : String = ""
+    }
+}
+
