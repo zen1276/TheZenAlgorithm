9f8fac8942668c61eae1eed9778c2daaeddbf36e
Van hoffman <zvan1276@740s-pl-119.local>
2018-10-23 09:05:26 -0600

    added the steps to make an app into the let variables, and make a loop to execute the algorithm.

--- TheZenAlgorithm/Controller/AlgorithmViewController.swift
+++ TheZenAlgorithm/Controller/AlgorithmViewController.swift
@@ -24,9 +24,51 @@ public class AlgorithmViewController: UIViewController
 
     private func formatAlgorithm() -> Void
     {
-        let stepOne : String = ""
-        let stepTwo : String = ""
-        let stepThree : String = ""
+        let title : String = "How to make an app in Xcode"
+        
+        let stepOne : String = "First, open up Xcode, and click on Create a new Xcode project. For our situation, we will be creating a Single View App, so click on that option."
+        let stepTwo : String = "Next we will need to create our MVC folders, so command click on LaunchScreen.storyboard and on Main.storyboard then right click and choose \"New group from selection\" then rename this new group to be \"View\""
+        let stepThree : String = "Now you will create your \"Controller\" folder by clicking on ViewController.swift and creating a new group from selection and renaming it \"Controller\"."
+        let stepFour : String = "Then create your resources folder, which will include assets.xcassets, info.plist, and appdelegate.swift."
+        let stepFive : String = "Finally, create a new repository of this app to github, and click undo so that you can go into the .gitignore where you will add \"*.xcuserstate\""
+        
+        let algorithm = [stepOne, stepTwo, stepThree, stepFour, stepFive]
+        
+        let attributesDictionary = [NSAttributedString.Key.font : algorithmText.font]
+        let full AttributedString = NSMutableAttributedString(string: title, attributes: attributesDictionary)
+        
+        for step in algorithm
+        {
+        let bullet : String = "🈸"
+            let formattedStep : String = "\n\(bullet) \(step)"
+            let attributedStringStep : NSMutableAttributedString = NSMutableAttributedString(string: formattedStep)
+            let outlineStyle = createOutlineStyle()
+            
+            attributedStringStep.addAttributes([NSAttributedString.Key.paragraphStyle : outlineStyle], range: NSMakeRange(0,attributedStringStep.length))
+            
+            fullAttributedString.append(attributedStringStep)
+        }
+        
+        algorithmText.attributedText = fullAttributedString
+        
     }
+    
+    private func createOutlineStyle() -> NSParagraphStyle
+    {
+        let outlineStyle : NSMutableParagraphStyle = NSMutableParagraphStyle()
+        
+        outlineStyle.alignment = .left
+        outlineStyle.defaultTabInterval = 15
+        outlineStyle.firstLineHeadIndent = 20
+        outlineStyle.headIndent = 35
+        
+        return outlineStyle
+    }
+    
+    
+    
+    
+    
+    
 }
 
